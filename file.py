2f37d765899ea29be2ed3866e88bf91795139918
Percival33 <marcin.jarc@gmail.com>
2022-05-23 23:23:07 +0200

    greeter decorator

--- file.py
+++ file.py
@@ -1,5 +1,10 @@
 def greeter(func):
-    pass
+    def inner(*args):
+        names_str = func(*args)
+        names = [ name.capitalize() for name in names_str.split(" ")]
+        names.insert(0, "Aloha")
+        return " ".join(names)
+    return inner
 
 
 def sums_of_str_elements_are_equal(func):
