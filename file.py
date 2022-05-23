656d06b7b4ea05610afb2c911a14b7fcd6b65ce7
Percival33 <marcin.jarc@gmail.com>
2022-05-23 23:38:29 +0200

    sum_of_str_elements_are_equal decorator

--- file.py
+++ file.py
@@ -8,7 +8,31 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def get_sum(number):
+        sign = -1 if number[0] == "-" else 1
+        
+        sum_of_elements = 0
+
+        for digit in number:
+            if digit == "-":
+                continue
+            sum_of_elements += sign * int(digit)
+
+        return sum_of_elements
+
+    def inner(*args):
+        numbers = func(*args).split(" ")
+        sumA = get_sum(numbers[0])
+        sumB = get_sum(numbers[1])
+
+        if sumA == sumB:
+            text = f"{sumA} == {sumB}"
+        else:
+            text = f"{sumA} != {sumB}"
+
+        return text
+
+    return inner
 
 
 def format_output(*required_keys):
